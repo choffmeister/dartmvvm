87d53b857ce9de760348771597071da2d8340fd7
choffmeister <thekwasti@googlemail.com>
2012-07-06 18:29:35 +0200

    improved value binding

--- Bindings/ValueBinding.dart
+++ Bindings/ValueBinding.dart
@@ -2,12 +2,14 @@ class ValueBinding extends BindingBase {
   InputElement _inputElement;
   // due to a bug (see http://code.google.com/p/dart/issues/detail?id=144)
   Function _elementChanged2;
+  Function _elementKeyboard2;
 
   ValueBinding(ViewModelBinder vmb, BindingGroup bg, BindingDescription desc)
     : super(vmb, bg, desc)
   {
     _inputElement = element;
     _elementChanged2 = _elementChanged;
+    _elementKeyboard2 = _elementKeyboard;
   }
 
   void onBind() {
@@ -25,5 +27,11 @@ class ValueBinding extends BindingBase {
 
   void _elementChanged(Event event) {
     modelValue = _inputElement.value;
+  }
+
+  void _elementKeyboard(KeyboardEvent event) {
+    if (event.keyCode == 13) {
+      modelValue = _inputElement.value;
+    }
   }
 }
